c185c33952ba9d904c3caa3757f0a0723e182489
lohanvictor <lohan.victor@hotmail.com>
2024-10-07 19:11:51 -0400

    refactor: modal when click in edit

--- app/personas/[id]/view/_components/EditButton/index.tsx
+++ app/personas/[id]/view/_components/EditButton/index.tsx
@@ -1,23 +1,17 @@
 "use client";
 
 import { PersonaModel } from "@/app/@types/persona.type";
-import Modal from "@/app/_components/Modal/Modal";
+import { ptBr } from "@/app/_lang/ptBr";
 import { LocalStorageUtils } from "@/app/_utils/localStorage.util";
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 type EditButtonProps = {
   persona: PersonaModel;
 };
 
 export default function EditButton({ persona }: EditButtonProps) {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   const route = useRouter();
-
-  function toggleModal() {
-    setIsOpenModal((prev) => !prev);
-  }
 
   function onProceed() {
     LocalStorageUtils.set("mapaut:persona" + persona.id, persona);
@@ -25,18 +19,8 @@ export default function EditButton({ persona }: EditButtonProps) {
   }
 
   return (
-    <>
-      <Button onClick={onProceed} variant="contained" color="primary">
-        Editar
-      </Button>
-
-      <Modal
-        isOpen={isOpenModal}
-        onCancel={toggleModal}
-        onProceed={onProceed}
-        text="Você deseja editar esta persona?"
-        type="normal"
-      />
-    </>
+    <Button onClick={onProceed} variant="contained" color="primary">
+      Editar
+    </Button>
   );
 }
