d8b1c515c20cd6bf5cddd4325f15f4da77ff3a0b
lohanvictor <lohan.victor@hotmail.com>
2024-10-22 14:27:04 -0400

    refactor: navbar

--- app/_components/Navbar/NavbarButton/index.tsx
+++ app/_components/Navbar/NavbarButton/index.tsx
@@ -12,11 +12,13 @@ export const NavbarButton = (props: NavbarButtonProps) => {
   return (
     <Link
       href={props.pathname}
-      className={`flex flex-col items-center justify-center w-full h-10 bg-sky-500 hover:bg-sky-400 cursor-pointer ${
-        props.isActive ? "bg-sky-800" : ""
+      className={`flex flex-col items-center justify-center w-full h-10 hover:bg-sky-200 cursor-pointer ${
+        props.isActive ? "bg-sky-800" : "bg-transparent"
       }`}
     >
-      <span className="text-white">{props.name}</span>
+      <span className={props.isActive ? "text-white" : "text-sky-800 font-semibold"}>
+        {props.name}
+      </span>
     </Link>
   );
 };
--- app/_components/Navbar/index.tsx
+++ app/_components/Navbar/index.tsx
@@ -8,18 +8,22 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="flex flex-col w-2/12 h-full bg-sky-950 gap-1 max-w-56 min-w-56">
-      <div className="flex flex-col items-center justify-center w-full h-20 bg-sky-950  ">
-        <span className="text-white">MapAut</span>
+    <nav className="flex flex-col w-2/12 h-full bg-transparent gap-1 max-w-56 min-w-56 border-r border-gray-400">
+      <div className="flex flex-col items-center justify-center w-full pb-4 pt-4">
+        <span className="text-slate-950 text-4xl">
+          <strong>MapAut</strong>
+        </span>
       </div>
-      {NAVBAR_ITEMS.map((item) => (
-        <NavbarButton
-          key={item.pathname}
-          name={item.name}
-          pathname={item.pathname}
-          isActive={pathname === item.pathname}
-        />
-      ))}
+      <div className="pl-4 flex flex-col gap-2">
+        {NAVBAR_ITEMS.map((item) => (
+          <NavbarButton
+            key={item.pathname}
+            name={item.name}
+            pathname={item.pathname}
+            isActive={pathname === item.pathname}
+          />
+        ))}
+      </div>
     </nav>
   );
 };
